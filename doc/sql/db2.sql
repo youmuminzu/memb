9755cb76c594dc5dbb3d7cc275da5ba386f5b9a6
youmuminzu <yufume@163.com>
2018-07-09 19:32:32 +0800

    add some configuration

---
+++ doc/sql/db2.sql
@@ -0,0 +1,30 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : local
+Source Server Version : 80011
+Source Host           : localhost:3306
+Source Database       : db2
+
+Target Server Type    : MYSQL
+Target Server Version : 80011
+File Encoding         : 65001
+
+Date: 2018-07-09 19:29:12
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `user_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `password` char(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `salt` char(5) NOT NULL,
+  `create_time` timestamp NULL DEFAULT NULL,
+  `update_time` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
