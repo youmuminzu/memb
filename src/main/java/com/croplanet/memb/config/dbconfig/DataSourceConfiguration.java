b38d4741aff834ae658ba27ff58cdca68d08c005
youmuminzu <yufume@163.com>
2018-07-13 17:32:32 +0800

    add some configuration files

--- src/main/java/com/croplanet/memb/config/dbconfig/DataSourceConfiguration.java
+++ src/main/java/com/croplanet/memb/config/dbconfig/DataSourceConfiguration.java
@@ -1,4 +1,40 @@
 package com.croplanet.memb.config.dbconfig;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
 public class DataSourceConfiguration {
+
+    @Value("${mysql.dataSourceType}")
+    private Class<? extends DataSource> dataSourceType;
+
+    @Bean(name = "dataSourceOne")
+    @Primary
+    @ConfigurationProperties(prefix = "mysql.db1")
+    public DataSource dataSourceOne() {
+        return DataSourceBuilder.create().type(dataSourceType).build();
+    }
+
+    @Bean(name = "dataSourceTwo")
+    @Primary
+    @ConfigurationProperties(prefix = "mysql.db2")
+    public DataSource dataSourceTwo() {
+        return DataSourceBuilder.create().type(dataSourceType).build();
+    }
+
+    @Bean(name = "dataSourceSystem")
+    @Primary
+    @ConfigurationProperties(prefix = "mysql.dbSystem")
+    public DataSource dataSourceSystem() {
+        return DataSourceBuilder.create().type(dataSourceType).build();
+    }
+
+
 }
--- src/main/java/com/croplanet/memb/config/dbconfig/MybatisConfiguration.java
+++ src/main/java/com/croplanet/memb/config/dbconfig/MybatisConfiguration.java
@@ -1,4 +1,38 @@
 package com.croplanet.memb.config.dbconfig;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+@AutoConfigureAfter
+@MapperScan({"com.croplanet.memb.dao"})
 public class MybatisConfiguration {
+
+    @Value("${mybatis.mapperLocation}")
+    private String mapperLocation;
+
+    @Value("${mybatis.configLocation}")
+    private String configLocation;
+
+    @Qualifier("dataSourceOne")
+    @Autowired
+    private DataSource dataSourceOne;
+
+    @Qualifier("dataSourceTwo")
+    @Autowired
+    private DataSource dataSourceTwo;
+
+
+    @Qualifier("dataSourceSystem")
+    @Autowired
+    private DataSource dataSourceSystem;
+
+
+
 }
